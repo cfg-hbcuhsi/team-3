357a4361750396ceef36a15a776039acfccf7f29
alvinjiang32 <36828072+alvinjiang32@users.noreply.github.com>
2020-09-12 05:52:45 -0700

    Recreated navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
-import { Nav, Navbar, Form, Button, FormControl } from "react-bootstrap";
 import Biographies from "./Components/Biographies";
 import Mentoring from "./Components/Mentoring";
 import Resources from "./Components/Resources";
 import Home from "./Components/Home";
 import "./App.css";
+import AppNavBar from './Components/AppNavBar';
+
 
 class App extends Component {
   render() {
@@ -14,21 +14,7 @@ class App extends Component {
       <Router>
         <div>
           <div>
-            <Navbar bg="primary" variant="dark">
-              <img
-                alt=""
-                src={require("./Components/ourLogo.png")}
-                width="50"
-                height="50"
-                className="d-inline-block align-top"
-              />
-              <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/mentoring">Mentoring</Nav.Link>
-                <Nav.Link href="/biographies">Biographies</Nav.Link>
-                <Nav.Link href="/resources">Resources</Nav.Link>
-              </Nav>
-            </Navbar>
+            <AppNavBar />
           </div>
           <Switch>
             <Route path="/mentoring">
