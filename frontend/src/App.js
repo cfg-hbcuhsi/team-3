14dbea42731e25a0c29ccda52f99c99ca74d89fa
Victor <vfagbolu@gmail.com>
2020-09-11 21:25:55 -0500

    made a nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
-
+import { Nav, Navbar, Form, Button, FormControl } from "react-bootstrap";
 import Biographies from "./Components/Biographies";
 import Mentoring from "./Components/Mentoring";
 import Resources from "./Components/Resources";
@@ -12,6 +12,14 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Navbar>
           <nav>
             <ul>
               <li>
