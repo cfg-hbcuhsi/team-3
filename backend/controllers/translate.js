394b1506f54612ac8f9eaaec6302d7cb8b19a1eb
Anthony Bravo <abravo3641@gmail.com>
2020-09-11 23:24:28 -0400

    done - route for translation

---
+++ backend/controllers/translate.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const AWS = require('aws-sdk');
+const translate = new AWS.Translate();
+
+router.get('/', (req, res) => {
+    const {sourceLanguage, targetLanguage, text} = req.body;
+    const params = {
+        SourceLanguageCode: sourceLanguage,
+        TargetLanguageCode: targetLanguage,
+        Text: text
+    }
+
+    translate.translateText(params, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        res.json({text: data.TranslatedText});
+    })
+})
+
+module.exports = router;
