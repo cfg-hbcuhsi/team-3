50fe8377a04975a0b863bc1ed3228dcda736bb90
alvinjiang32 <36828072+alvinjiang32@users.noreply.github.com>
2020-09-11 20:52:47 -0700

    centered everything

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,44 +6,47 @@ import Biographies from "./Components/Biographies";
 import Mentoring from "./Components/Mentoring";
 import Resources from "./Components/Resources";
 import Home from "./Components/Home";
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/mentoring">Mentoring</Link>
-              </li>
-              <li>
-                <Link to="/biographies">Biographies</Link>
-              </li>
-              <li>
-                <Link to="/resources">Resources</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/mentoring">
-              <Biographies />
-            </Route>
-            <Route path="/biographies">
-              <Mentoring />
-            </Route>
-            <Route path="/resources">
-              <Resources />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <div className="App">
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/mentoring">Mentoring</Link>
+                </li>
+                <li>
+                  <Link to="/biographies">Biographies</Link>
+                </li>
+                <li>
+                  <Link to="/resources">Resources</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/mentoring">
+                <Biographies />
+              </Route>
+              <Route path="/biographies">
+                <Mentoring />
+              </Route>
+              <Route path="/resources">
+                <Resources />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
     );
   }
 }
