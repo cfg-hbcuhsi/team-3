70386ed8e3c3ff97a93a2c441f7a5146dbb7e35c
Anthony Bravo <abravo3641@gmail.com>
2020-09-12 00:10:57 -0400

    starting resources route

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const testing = require('./testing');
 const translate = require('./translate');
+const resources = require('./resources');
 
 router.use('/testing', testing);
 router.use('/translate', translate);
+router.use('/resources', resources);
 
 module.exports = router;
