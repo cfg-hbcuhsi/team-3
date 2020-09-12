bbd35f098194a3f10180204f40a53627d7181ab6
Victor <vfagbolu@gmail.com>
2020-09-12 04:31:21 -0500

    added searching

--- frontend/src/Components/videoCard.jsx
+++ frontend/src/Components/videoCard.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios";
 import {
   Form,
   Button,
   FormControl,
   Card,
   ResponsiveEmbed,
+  Modal,
 } from "react-bootstrap";
 import "../App.css";
 
 class Video extends Component {
+  state = {
+    show: false,
+  };
+  handleQuestion = () => {
+    axios
+      .post("http://localhost:4000/api/videos", { id: 1, question: "1232131" })
+      .then((res) => this.handleClose())
+      .catch(
+        (err) =>
+          (document.getElementById("alert-status").innerText =
+            "Failed to Upload")
+      );
+  };
+  handleClose = () => this.setState({ show: false });
+  handleShow = () => this.setState({ show: true });
+
   render() {
     const {
       id,
@@ -19,6 +37,7 @@ class Video extends Component {
       upload_date,
       description,
     } = this.props.data;
+    const { show } = this.state;
     return (
       <div>
         <div></div>
@@ -34,9 +53,37 @@ class Video extends Component {
           <Card.Body className="cardBody">
             <Card.Title>{title}</Card.Title>
             <Card.Text>{description}</Card.Text>
-            <Button variant="primary">{"Ask " + mentor_name}</Button>
+            <Button variant="primary" onClick={() => this.handleShow()}>
+              {"Ask " + mentor_name}
+            </Button>
+            <Modal
+              show={show}
+              onHide={() => this.handleClose()}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>Ask {mentor_name} your question</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <textarea
+                  style={{ width: "100%" }}
+                  id="question-text"
+                ></textarea>
+              </Modal.Body>
+              <Modal.Footer>
+                <div id="alert-status"></div>
+                <Button variant="secondary" onClick={() => this.handleClose()}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={() => this.handleQuestion()}>
+                  Send Message
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Card.Body>
         </Card>
+        <br />
       </div>
     );
   }
