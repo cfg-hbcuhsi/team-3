166468276f01aa25319938b6510ee67a2acdbc79
Minh Nguyen <tmnguyen1403@gmail.com>
2020-09-11 22:33:11 -0500

    Implement route for video 1.	Get video based 2. Ask question based on video id

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router  = express.Router();
 const testing = require('./testing');
+const videos = require('./videos');
 const translate = require('./translate');
 const mentor  = require('./mentors');
 
 router.use('/testing', testing);
 router.use('/mentor', mentor);
 router.use('/translate', translate);
+router.use('/videos', videos);
+
 
 module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = 4000;
 
 app.use(bodyParser.json());
+
 app.use('/api', require('./controllers'));
 
 app.listen(PORT, () => { console.log(`Running on port: ${PORT}`) });
