4f1a2605eb027fce119ad1f81d069ef892e8d0a1
Victor <vfagbolu@gmail.com>
2020-09-12 06:26:22 -0500

    back end data push

--- backend/data/videos.js
+++ backend/data/videos.js
@@ -15,6 +15,78 @@ const data = [{
 	mentor_name: "Dr. Sheperd",
 	upload_date : "09-12-2020",//mm-dd-yyy
     description: "Dr. Sheperd talks about life as a doctor at Hopkins"
-}]
+},
+{
+	id: 7,
+	title: "The Process From Zero Programming Knowledge to Software Development Job",
+	profession: "Software Developer",
+	url: "https://www.youtube.com/embed/yufRLksxdLo",
+	mentor_name: "Mr. Forrest",
+	upload_date : "02-08-2019",//mm-dd-yyy
+	description: "Let's take a look at the overall workflow of going from zero programming knowledge to landing that job as a software developer. There are thousands of different caveats and ways to accomplish this, but this is based on my personal experience and what I've learned along the way."
+},
+{
+	id: 8,
+	title: "What Does a Project Manager Do [THE ROLE OF THE PM]",
+	profession: "Program Manager",
+	url: "https://www.youtube.com/embed/gu-z9EbIZ5k",
+	mentor_name: "Adriana Girdler",
+	upload_date : "02-12-2020",//mm-dd-yyy
+	description: "What Does a Project Manager Do / Are you wondering, “what does a project manager do?” In today’s video, I’m going to give you a clear project manager description that will answer the question, “what is a project manager?” So, if you’re hoping to understand the role of the project manager, I have great news for you... By the end of this video, you’ll understand project manager responsibilities!"
+},
+{
+	id: 9,
+	title: "How I Would Learn Data Science (If I Had to Start Over)",
+	profession: "Data Scientist",
+	url: "https://www.youtube.com/embed/4OZip0cgOho",
+	mentor_name: "Ken Jee",
+	upload_date : "05-08-2020",//mm-dd-yyy
+	description: "In this video, I talk about how I would learn data science if I had to start again from scratch. Learning data science and machine learning is a little bit different for everyone, but hopefully this video will help you to build the necessary foundation to tackle these fields. "
+},
+    {
+	id: 10,
+	title: "How to become news anchor in India || full course detail in Hindi",
+	profession: "News Anchor",
+	url: "https://www.youtube.com/embed/Hfmi0KED0TY",
+	mentor_name: "Ken Jee",
+	upload_date : "05-12-2018",//mm-dd-yyy
+	description: "In this video we will talk about: How to become a news anchor in India, some required qualities for news anchor, educational qualification, institutions, salary"
+},
+{
+	id: 20,
+	title: "Becoming a Chef",
+	profession: "Chef",
+	url:  "https://www.youtube.com/embed/FAfXzuwKhOQ",
+	mentor_name: "Mr. Jeff",
+	upload_date : "09-11-2020",//mm-dd-yyy
+	Description: "I am a chef"
+},{
+	id: 21,
+	title: "TedxPilly - A chef's story",
+	profession: "Chef",
+	url:  "https://www.youtube.com/embed/bhUmVz4-3-s",
+	mentor_name: "Mr. Michael Solomonov",
+	upload_date : "09-11-2020",//mm-dd-yyy
+	Description: "I am a chef"
+},{
+	id: 22,
+	title: "17 high-paying jobs for people who love history",
+	profession: "History work",
+	url:  "https://www.youtube.com/embed/7z2KQlqSoLs",
+	mentor_name: "Check Fact 360",
+	upload_date : "09-11-2020",//mm-dd-yyy
+	Description: "I am a historian"
+},
+{
+	id: 24,
+	title: "WHAT ITS LIKE TO MAJOR IN HISTORY |All About Studying History in College| Classes, Job Prospects,etc",
+	profession: "History work",
+	url:  "https://www.youtube.com/embed/bD1zW68DXfI",
+	mentor_name: "Mr. Michael Solomonov",
+	upload_date : "09-11-2020",//mm-dd-yyy
+	Description: "Make money from history"
+},
+
+]
 
 module.exports = data
